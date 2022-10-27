2dfc020dcd9c42e83d298b1c62231094ccfe501f
Tsion-Tadesse <46496704+Tsion-Tadesse@users.noreply.github.com>
2022-10-27 17:38:38 -0400

    Transfered the project from different repo

---
+++ BANKING/src/main/java/com/learning/entity/User.java
@@ -0,0 +1,77 @@
+package com.learning.entity;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="USERS")
+public class User {
+/*
+ * id: Number,
+ * username:String,
+ * fullname:String,
+ * phone: String,
+ * SSN: int,
+ * password: String 
+ * role: enum RoleType (CUSTOMER, STAFF, ADMIN)
+ */
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	@Column(name="username")
+	private String username;
+	@Column(name="full_name")
+	private String fullName;
+	@Column(name="phone")
+	private String phone;
+	@Column(name="ssn")
+	private int ssn;
+	@Column(name="password")
+	private String password;
+	
+	public User() {
+		super();
+	}
+
+	public User(long id, String username, String fullName, String password) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.fullName = fullName;
+		this.password = password;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
