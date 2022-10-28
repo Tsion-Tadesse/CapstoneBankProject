0d5311bc78aa4641d93cfd72bc90497cd7eb3bd0
VADIM <79195442+xrustlex@users.noreply.github.com>
2022-10-28 12:03:38 -0400

    repo done

--- BANKING/src/main/java/com/learning/repo/PasswordRepository.java
+++ BANKING/src/main/java/com/learning/repo/PasswordRepository.java
@@ -2,6 +2,8 @@ package com.learning.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.learning.entity.Password;
+
 public interface PasswordRepository extends JpaRepository<Password, Long> {
 
 }
