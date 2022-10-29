5dcd6a7bf17c6704ccc725d4c93ac86c9496e9f3
Tsion-Tadesse <46496704+Tsion-Tadesse@users.noreply.github.com>
2022-10-28 20:57:36 -0400

    minor change

--- BANKING/src/main/java/com/learning/BankingApplication.java
+++ BANKING/src/main/java/com/learning/BankingApplication.java
@@ -8,10 +8,6 @@ public class BankingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankingApplication.class, args);
-<<<<<<< HEAD
-		System.out.println("Welcome to the Banking Application");
-=======
->>>>>>> 9957d9e5059fd99669887b32340cceda8c42f8b4
 	}
 
 }
